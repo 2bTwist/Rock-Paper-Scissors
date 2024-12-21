eabd0e399ff4990e3a0457f863d436859f797f37
2bTwist <ndanjiedmond@gmail.com>
2024-12-21 12:33:41 -0500

    Added 5 Rounds with point system

--- script.js
+++ script.js
@@ -28,53 +28,78 @@ function getHumanChoice(){
     }
 }
 
-function playRound(humanChoice, computerChoice){
-    console.log(`You played: ${humanChoice}, Computer Played: ${computerChoice}`)
-
-    if (humanChoice == "Rock"){
-        if(computerChoice == "Rock"){
-            console.log(`You both played ${humanChoice}`)
-        }
-        else if(computerChoice == "Paper"){
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        }
-        else if(computerChoice == "Scissors"){
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-        }
-    }
-
-    else if (humanChoice == "Paper"){
-        if(computerChoice == "Rock"){
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-        }
-        else if(computerChoice == "Paper"){
-            console.log(`You both played ${humanChoice}`)
-        }
-        else if(computerChoice == "Scissors"){
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        }
-    }
-
-    else if (humanChoice == "Scissors"){
-        if(computerChoice == "Rock"){
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        }
-        else if(computerChoice == "Paper"){
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-        }
-        else if(computerChoice == "Scissors"){
-            console.log(`You both played ${humanChoice}`)
-        }
-    }
-}
-
-
 function playGame(){
+
+    var humanPoints = 0
+    var computerPoints = 0
+
+    function playRound(humanChoice, computerChoice){
+        console.log(`You played: ${humanChoice}, Computer Played: ${computerChoice}`)
     
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+        if (humanChoice == "Rock"){
+            if(computerChoice == "Rock"){
+                console.log(`You both played ${humanChoice}`)
+            }
+            else if(computerChoice == "Paper"){
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                computerPoints = computerPoints + 1
+            }
+            else if(computerChoice == "Scissors"){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                humanPoints = humanPoints + 1
+            }
+        }
     
-    playRound(humanSelection, computerSelection);
+        else if (humanChoice == "Paper"){
+            if(computerChoice == "Rock"){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                humanPoints = humanPoints + 1
+            }
+            else if(computerChoice == "Paper"){
+                console.log(`You both played ${humanChoice}`)
+            }
+            else if(computerChoice == "Scissors"){
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                computerPoints = computerPoints + 1
+            }
+        }
+    
+        else if (humanChoice == "Scissors"){
+            if(computerChoice == "Rock"){
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+                computerPoints = computerPoints + 1
+            }
+            else if(computerChoice == "Paper"){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                humanPoints = humanPoints + 1
+            }
+            else if(computerChoice == "Scissors"){
+                console.log(`You both played ${humanChoice}`)
+            }
+        }
+    }
+    
+
+    for (let i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+        console.log(`Human: ${humanPoints}, Computer: ${computerPoints}`)
+    }
+
+    if(humanPoints > computerPoints){
+        console.log(`YOU WON, You have ${humanPoints}pts`)
+    }
+    else if(humanPoints < computerPoints){
+        console.log(`YOU LOSE, The Computer has ${computerPoints}pts`)
+    }
+    else{
+        console.log(`IT's A DRAW, you both have ${humanPoints}pts`)
+    }
 }
+
+// playGame() //Uncomment to play game
 
 
