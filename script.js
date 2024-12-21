4fcc1f075b1719b510d6c040ac3c39f404cec48f
2bTwist <ndanjiedmond@gmail.com>
2024-12-21 12:08:35 -0500

    Implemented game logic

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3); // either 0, 1, 2
+
+    if (choice === 0){
+        return "Rock";
+    }
+    else if (choice === 1){
+        return "Paper";
+    }
+    else {
+        return "Scissors"
+    }
+}
+
+function getHumanChoice(){
+    let choice = parseInt(prompt("Make your choice \n 0:Rock 1:Paper 2:Scissors"))
+
+    if (choice === 0){
+        return "Rock";
+    }
+    else if (choice === 1){
+        return "Paper";
+    }
+    else {
+        return "Scissors"
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    console.log(`You played: ${humanChoice}, Computer Played: ${computerChoice}`)
+
+    if (humanChoice == "Rock"){
+        if(computerChoice == "Rock"){
+            console.log(`You both played ${humanChoice}`)
+        }
+        else if(computerChoice == "Paper"){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+        else if(computerChoice == "Scissors"){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        }
+    }
+
+    else if (humanChoice == "Paper"){
+        if(computerChoice == "Rock"){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        }
+        else if(computerChoice == "Paper"){
+            console.log(`You both played ${humanChoice}`)
+        }
+        else if(computerChoice == "Scissors"){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+    }
+
+    else if (humanChoice == "Scissors"){
+        if(computerChoice == "Rock"){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+        else if(computerChoice == "Paper"){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        }
+        else if(computerChoice == "Scissors"){
+            console.log(`You both played ${humanChoice}`)
+        }
+    }
+}
+
+
+function playGame(){
+    
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    
+    playRound(humanSelection, computerSelection);
+}
+
+
